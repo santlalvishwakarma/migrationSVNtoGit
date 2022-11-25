94598d7f6475e6d48a2173e05adb0ec471270a64
santlalvishwakarma <vsantlal@yahoo.com>
2022-11-25 14:00:29 +0530

    Testing

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -5,5 +5,9 @@ public class SumOfNumbers {
 		int n1 = 225, n2 = 115, sum;
 		sum = n1 + n2;
 		System.out.println("The sum of numbers is: " + sum);
+		
+		System.out.println("Testing");
+		
+		
 	}
 }
